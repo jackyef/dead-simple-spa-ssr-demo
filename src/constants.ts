823ccbfada0e02abbab5bd191520e544251a4ee0
jackyef <jacky.efendi1@gmail.com>
2023-12-30 19:45:43 +0700

    refactor and comment

--- src/constants.ts
+++ src/constants.ts
@@ -9,3 +9,64 @@ export const homeHtml = (prefix: string) => `
 
 <a href="/${prefix}/blog">Go to blog</a>
 `
+
+export const uiLibraryScript = (prefix: string) => `
+<script>
+  const root = document.getElementById('app')
+
+  if (window.location.pathname === '/${prefix}') {
+    root.innerHTML = \`${homeHtml(prefix)}\`
+  }
+
+  if (window.location.pathname === '/${prefix}/blog') {
+    root.innerHTML = \`${blogHtml(prefix)}\`
+  }
+
+  const renderPage = (path) => {
+    if (path === '/${prefix}') {
+      root.innerHTML = \`${homeHtml(prefix)}\`
+
+      return
+    }
+
+    if (path === '/${prefix}/blog') {
+      root.innerHTML = \`${blogHtml(prefix)}\`
+
+      return
+    }
+
+    root.innerHTML = \`<h1>404</h1>\`
+  }
+
+  document.addEventListener('click', (e) => {
+    const isAnchor = e.target.tagName === 'A'
+
+    if (!isAnchor) {
+      return
+    }
+
+    e.preventDefault()
+
+    const href = e.target.getAttribute('href')
+    const current = window.location.pathname
+    
+    if (href === current) return
+    
+    if (href === '/${prefix}') {
+      window.history.pushState({}, '', '/${prefix}')
+    }
+
+    if (href === '/${prefix}/blog') {
+      window.history.pushState({}, '', '/${prefix}/blog')
+    }
+
+    renderPage(href)
+  })
+
+  window.addEventListener('popstate', (event) => {
+    const destination = window.location.pathname
+    
+    renderPage(destination)
+  })
+</script>
+`
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express'
-import { blogHtml, homeHtml } from './constants'
+import { blogHtml, homeHtml, uiLibraryScript } from './constants'
 
 const app = express()
 
+// How OG websites work
+// - Separate route handlers for each paths
+// - Construct HTML string and send it back
 app.get('/original', (req, res) => {
   res.send(`
     <html>
@@ -23,7 +26,8 @@ app.get('/original/blog', (req, res) => {
   `)
 })
 
-
+// SPA with SSR
+// - Basically the same as SPA, but the initial HTML isn't empty. We render on the server once before sending it back.
 app.get('/spa-ssr*', (req, res) => {
   const renderHtml = (path: string) => {
     if (path.endsWith('/blog')) {
@@ -41,68 +45,14 @@ app.get('/spa-ssr*', (req, res) => {
         </div>
       </body>
 
-      <script>
-        const root = document.getElementById('app')
-
-        if (window.location.pathname === '/spa-ssr') {
-          root.innerHTML = \`${homeHtml('spa-ssr')}\`
-        }
-
-        if (window.location.pathname === '/spa-ssr/blog') {
-          root.innerHTML = \`${blogHtml('spa-ssr')}\`
-        }
-
-        const renderPage = (path) => {
-          if (path === '/spa-ssr') {
-            root.innerHTML = \`${homeHtml('spa-ssr')}\`
-
-            return
-          }
-
-          if (path === '/spa-ssr/blog') {
-            root.innerHTML = \`${blogHtml('spa-ssr')}\`
-
-            return
-          }
-
-          root.innerHTML = \`<h1>404</h1>\`
-        }
-
-        document.addEventListener('click', (e) => {
-          const isAnchor = e.target.tagName === 'A'
-
-          if (!isAnchor) {
-            return
-          }
-
-          e.preventDefault()
-
-          const href = e.target.getAttribute('href')
-          const current = window.location.pathname
-          
-          if (href === current) return
-          
-          if (href === '/spa-ssr') {
-            window.history.pushState({}, '', '/spa-ssr')
-          }
-
-          if (href === '/spa-ssr/blog') {
-            window.history.pushState({}, '', '/spa-ssr/blog')
-          }
-
-          renderPage(href)
-        })
-
-        window.addEventListener('popstate', (event) => {
-          const destination = window.location.pathname
-          
-          renderPage(destination)
-        })
-      </script>
+      ${uiLibraryScript('spa-ssr')}
     </html>
   `)
 })
 
+// The early days SPA (e.g.: the thing everyone is doing by following create-react-app)
+// - Empty HTML document with script tag
+// - JS will modify the HTML with content once JS runs on the browser
 app.get('/spa*', (req, res) => {
   res.send(`
     <html>
@@ -110,64 +60,7 @@ app.get('/spa*', (req, res) => {
         <div id="app"></div>
       </body>
 
-      <script>
-        const root = document.getElementById('app')
-
-        if (window.location.pathname === '/spa') {
-          root.innerHTML = \`${homeHtml('spa')}\`
-        }
-
-        if (window.location.pathname === '/spa/blog') {
-          root.innerHTML = \`${blogHtml('spa')}\`
-        }
-
-        const renderPage = (path) => {
-          if (path === '/spa') {
-            root.innerHTML = \`${homeHtml('spa')}\`
-
-            return
-          }
-
-          if (path === '/spa/blog') {
-            root.innerHTML = \`${blogHtml('spa')}\`
-
-            return
-          }
-
-          root.innerHTML = \`<h1>404</h1>\`
-        }
-
-        document.addEventListener('click', (e) => {
-          const isAnchor = e.target.tagName === 'A'
-
-          if (!isAnchor) {
-            return
-          }
-
-          e.preventDefault()
-
-          const href = e.target.getAttribute('href')
-          const current = window.location.pathname
-          
-          if (href === current) return
-          
-          if (href === '/spa') {
-            window.history.pushState({}, '', '/spa')
-          }
-
-          if (href === '/spa/blog') {
-            window.history.pushState({}, '', '/spa/blog')
-          }
-
-          renderPage(href)
-        })
-
-        window.addEventListener('popstate', (event) => {
-          const destination = window.location.pathname
-          
-          renderPage(destination)
-        })
-      </script>
+      ${uiLibraryScript('spa')}
     </html>
   `)
 })
